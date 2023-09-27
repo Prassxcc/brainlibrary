a81253fd79f678936c24520aa02d1959bc2005af
Prassxcc <106374957+Prassxcc@users.noreply.github.com>
2023-09-27 07:54:37 +0700

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <header class="py-3 mb-3 border-bottom">
+        <div class="container-fluid d-grid gap-3 align-items-center" style="grid-template-columns: 1fr 2fr;">
+          <div class="navbar-nav">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none ">
+                <img src="bb.jpg" alt="Logo" width="50" height="50"
+                    class="d-inline-block align-text-top" style="margin-right: auto; border-radius:50%; margin-left: 40px">
+                    <div class="name-logo text-dark">BrainGroup</div>logo
+            </a>
+          </div>
+
+          <div class="d-flex align-items-center">
+            <form class="w-100 me-3" role="search">
+              <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+            </form>
+
+            <div class="flex-shrink-0 dropdown">
+              <div class="text-end" style="margin-right:40px;">
+                <button type="button" class="btn btn-outline-dark font-weight-bold me-2">Login</button>
+
+            </div>
+            </div>
+          </div>
+        </div>
+      </header>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</html>
